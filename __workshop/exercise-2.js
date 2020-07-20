e3166f031aad8a47b78d5d8d1b22d1f50c8e1366
Rupinder Bagri <RBagri94@hotmail.com>
2020-07-20 06:13:11 -0400

    Q2

--- __workshop/exercise-2.js
+++ __workshop/exercise-2.js
@@ -1,19 +1,37 @@
 // Exercise 2 - `getIssPosition`
 // -----------------------------
 
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getIssPosition = async () => {
   try {
-    const response = await request(/* fill this in */);
-    const issLocation = JSON.parse(/* fill this in */);
+    const response = await request("http://api.open-notify.org/iss-now.json");
+    const issLocation = JSON.parse(response);
     return {
-      lat: /* fill this in */,
-      lng: /* fill this in */,
+      lat: issLocation.iss_position.latitude,
+      lng: issLocation.iss_position.longitude,
     };
   } catch (err) {
-    console.log('Error: ', err);
+    console.log("Error: ", err);
   }
 };
+// getting PROMISE {<pending>}**
 
-console.log(getIssPosition());
+getIssPosition().then((res) => console.log(res));
+
+//my 3.6 original answer is below:
+// const request = require("request-promise");
+// Returns the current position of the ISS
+// const getIssPosition = (lat, lng) => {
+//   return request("http://api.open-notify.org/iss-now.json")
+//     .then((response) => JSON.parse(response))
+//     .then((parsedResponse) => {
+//       return {
+//         lng: parsedResponse.iss_position.longitude,
+//         lat: parsedResponse.iss_position.latitude,
+//       };
+//     });
+// };
+
+// write some code..
+// getIssPosition("").then((result) => console.log(result));
