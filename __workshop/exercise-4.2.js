2f02e2a08c24cde6db902c7f2cba1b13e7fc05cf
Rupinder Bagri <RBagri94@hotmail.com>
2020-07-20 06:13:27 -0400

    Q4.2

--- __workshop/exercise-4.2.js
+++ __workshop/exercise-4.2.js
@@ -1,5 +1,17 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getTronaldDumpQuote = async () => {
-  // write write write
+  try {
+    const response = await request({
+      uri: "https://api.tronalddump.io/random/quote",
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    return JSON.parse(response).value;
+  } catch (err) {
+    console.log("error: ", err);
+  } // write write write
 };
+
+getTronaldDumpQuote().then((res) => console.log(res));
