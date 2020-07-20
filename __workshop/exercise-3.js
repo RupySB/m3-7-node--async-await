67385c38ee84bf51b54ec3c7c31caff624a773e8
Rupinder Bagri <RBagri94@hotmail.com>
2020-07-20 06:13:16 -0400

    Q3

--- __workshop/exercise-3.js
+++ __workshop/exercise-3.js
@@ -2,11 +2,36 @@
 // ----------
 
 const doublesLater = (num) => {
-  // 1. waits 2 seconds (You could console a message here. It might make the 2 second wait easier ;)
-  // 2. returns the double of the num
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(num * 2);
+    }, 2000);
+  });
 };
+const handleSum = async (num) => {
+  try {
+    const uno = await doublesLater(num);
+    const dos = await doublesLater(uno);
+    const tres = await doublesLater(dos);
+    return uno + dos + tres;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// 1. waits 2 seconds (You could console a message here. It might make the 2 second wait easier ;)
+// 2. returns the double of the num
 
 // 3. handleSum function (async/await)
 
 // 4. verification
 handleSum(10).then((ans) => console.log(ans));
+
+// const handleHandleSum = async (num) => {
+//   try {
+//     const result = await handleSum(num);
+//     console.log(result);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
