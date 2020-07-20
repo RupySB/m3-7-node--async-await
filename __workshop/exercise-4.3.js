2bb6fc5ca58ffbc3b1bb3c8c421aad17bf74bd37
Rupinder Bagri <RBagri94@hotmail.com>
2020-07-20 06:13:33 -0400

    Q4.3

--- __workshop/exercise-4.3.js
+++ __workshop/exercise-4.3.js
@@ -1,5 +1,15 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 const getGeekJoke = async () => {
-  // ha hahahh
+  try {
+    const response = await request(
+      "https://geek-jokes.sameerkumar.website/api?format=json"
+    );
+    const getJokes = JSON.parse(response);
+    return getJokes.joke;
+  } catch (err) {
+    console.log("error: ", err);
+  }
 };
+
+getGeekJoke().then((res) => console.log(res));
