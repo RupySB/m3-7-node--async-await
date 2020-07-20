122a07b80ba1f99c6ee8eb9fd13f9868115c3699
Rupinder Bagri <RBagri94@hotmail.com>
2020-07-20 06:13:22 -0400

    Q4.1

--- __workshop/exercise-4.1.js
+++ __workshop/exercise-4.1.js
@@ -1,6 +1,14 @@
-const request = require('request-promise');
+const request = require("request-promise");
 
 // getDadJoke
+const getDadJoke = async () => {
+  let answer = await request({
+    uri: "https://icanhazdadjoke.com/",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  return JSON.parse(answer).joke;
+};
 
-// 4.1
-// getJoke().then((data) => console.log(data));
+getDadJoke().then((data) => console.log(data));
